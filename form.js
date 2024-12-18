6043242c1c4111e4c1f0235c6c540b38b49bbb8f
BraveGian <iu7bzg@gmail.com>
2024-12-18 10:39:57 +0000

    messaggio

---
+++ form.js
@@ -0,0 +1,11 @@
+function msgAlert(){
+    alert(" Grazie per aver complitato il modulo");
+}
+
+function cambioForm(){
+    document.getElementById('box').style.backgroundColor="tomato";
+    document.querySelector('h2').style.color="black";
+    document.getElementById('box').style.borderColor="green";
+   
+   
+}
