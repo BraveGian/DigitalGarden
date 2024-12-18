86b12da225a609d6e6931f59bac43a8821c9215c
BraveGian <iu7bzg@gmail.com>
2024-12-18 14:56:40 +0000

    messaggio

--- form.js
+++ form.js
@@ -6,6 +6,6 @@ function cambioForm(){
     document.getElementById('box').style.backgroundColor="tomato";
     document.querySelector('h2').style.color="black";
     document.getElementById('box').style.borderColor="green";
-   
-   
+    document.querySelector('.layout').style.borderRadius='5px';
+    document.querySelector('#invia_form').style.borderRadius='5px';
 }
