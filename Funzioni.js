2a735c7574212ff2e36d91571cf5fca458793160
BraveGian <iu7bzg@gmail.com>
2024-12-18 17:30:14 +0000

    messaggio

---
+++ Funzioni.js
@@ -0,0 +1,19 @@
+function operazione(num1,num2,num3){                          //funzione che svolge l operazione somma
+     
+    var risultato= num1+num2+num3;
+    return risultato;
+
+}
+
+function calcolaSomma(){
+
+    var numero1= parseInt(document.getElementById('NumeroUno').value);    //prendi i numeri dai tre campi input
+    var numero2= parseInt(document.getElementById('NumeroDue').value);
+    var numero3= parseInt(document.getElementById('NumeroTre').value);
+
+    var resulSomma=operazione (numero1,numero2,numero3);
+
+    document.getElementById('resulSomma').innerHTML= " Il risultato della somma è" + resulSomma;
+
+
+}
