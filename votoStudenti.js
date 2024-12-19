eae8d21256807b061da17310262a9fd39b912456
BraveGian <iu7bzg@gmail.com>
2024-12-19 16:29:54 +0000

    messaggio

--- votoStudenti.js
+++ votoStudenti.js
@@ -1,5 +1,23 @@
 function premiPulsante(){
-    
-   const pulsante= document.querySelector('button');
+   
+       document.addEventListener("click",inserisciElemento);
   
+  function inserisciElemento(){
+       
+     let archivio=[3]; 
+     const votazioneITALIANO= document.getElementById('votoIT').value;
+     const votazioneMATEMATICA= document.getElementById('votoMAT').value;
+     const votazioneCHIMICA= document.getElementById('votoCH').value;
+     const votazioneINGLESE= document.getElementById('votoEN').value;
+     console.log(votazioneITALIANO);
+     console.log(votazioneMATEMATICA);
+     console.log(votazioneCHIMICA);
+     console.log(votazioneINGLESE);
+     archivio.push(votazioneITALIANO,votazioneMATEMATICA,votazioneCHIMICA,votazioneINGLESE);
+    
+    
+     console.log(archivio);
+     
+   }
+      
 }
