8e0ae154315d6dff0f59da82e00cd886713d773a
BraveGian <iu7bzg@gmail.com>
2024-12-19 17:58:32 +0000

    messaggio

--- votoStudenti.js
+++ votoStudenti.js
@@ -3,21 +3,27 @@ function premiPulsante(){
        document.addEventListener("click",inserisciElemento);
   
   function inserisciElemento(){
-       
-     let archivio=[3]; 
-     const votazioneITALIANO= document.getElementById('votoIT').value;
-     const votazioneMATEMATICA= document.getElementById('votoMAT').value;
-     const votazioneCHIMICA= document.getElementById('votoCH').value;
-     const votazioneINGLESE= document.getElementById('votoEN').value;
+      let somma=0;
+     const archivio=[3]; 
+     const votazioneITALIANO=  parseFloat(document.getElementById('votoIT').value);
+     const votazioneMATEMATICA= parseFloat(document.getElementById('votoMAT').value);
+     const votazioneCHIMICA= parseFloat(document.getElementById('votoCH').value);
+     const votazioneINGLESE= parseFloat(document.getElementById('votoEN').value);
      console.log(votazioneITALIANO);
      console.log(votazioneMATEMATICA);
      console.log(votazioneCHIMICA);
      console.log(votazioneINGLESE);
      archivio.push(votazioneITALIANO,votazioneMATEMATICA,votazioneCHIMICA,votazioneINGLESE);
-    
-    
      console.log(archivio);
-     
-   }
-      
-}
+
+     for( let i=0; i<archivio.length;i++){
+         somma+=archivio[i];
+       }
+        let media=somma/archivio.length;
+        console.log(" la media dell alunno è: " + media);
+    }
+
+  }
+
+
+    
