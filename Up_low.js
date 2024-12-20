bf46b0cd9da9bbf853b90cbf10500062e59617b7
BraveGian <iu7bzg@gmail.com>
2024-12-20 17:50:12 +0000

    messaggio

--- Up_low.js
+++ Up_low.js
@@ -1,15 +1,17 @@
 function verificaParole(){
-    let arrX=[1];
-    let arrY=[1];
-    
+    let arrX=[];
+    let arrY=[];
+    let contatoreX;
+    let contatoreY;
     let x=document.getElementById('parola1').value;
-    arrX[0]= x.split("");
+            arrX[0]= x.split("");
+        
     console.log(arrX);
     let y= document.getElementById('parola2').value;
-    arrY[0]= y.split("");
-    console.log(arrY)
-    
-        console.log("la prima parola inserita è: " , x );
-        console.log("la seconda parola inserita è: ", y );
+            arrY[0]= y.split("");
+            
+    console.log(arrY);
+    console.log("la prima parola inserita è: " , x );
+    console.log("la seconda parola inserita è: ", y );
 
 }
