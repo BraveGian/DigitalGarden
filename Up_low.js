42969782fc24e049b4914210b2d07c39017c5e4b
BraveGian <iu7bzg@gmail.com>
2024-12-20 17:27:43 +0000

    messaggio

--- Up_low.js
+++ Up_low.js
@@ -1,5 +1,15 @@
-function leggiParole(){
+function verificaParole(){
+    let arrX=[1];
+    let arrY=[1];
+    
     let x=document.getElementById('parola1').value;
+    arrX[0]= x.split("");
+    console.log(arrX);
     let y= document.getElementById('parola2').value;
+    arrY[0]= y.split("");
+    console.log(arrY)
+    
+        console.log("la prima parola inserita è: " , x );
+        console.log("la seconda parola inserita è: ", y );
 
 }
