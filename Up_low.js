3bd1f1c587168ea328bc7dc800a910864c7c7a53
BraveGian <iu7bzg@gmail.com>
2024-12-22 10:24:22 +0000

    messaggio

--- Up_low.js
+++ Up_low.js
@@ -1,17 +1,36 @@
 function verificaParole(){
-    let arrX=[];
-    let arrY=[];
-    let contatoreX;
-    let contatoreY;
+    let arrX=[];      // creiamo un array dove inserire la prima parola nel primo input
+    let arrY=[];      // creaio un  array dove inserire la seconda parola nel secondo input
+    let contatoreX=0;  // creiamo un contatore e lo inizializzo a 0 
+    let contatoreY=0;
+    let contMinX=0;
+    let contMinY=0;
     let x=document.getElementById('parola1').value;
-            arrX[0]= x.split("");
-        
-    console.log(arrX);
     let y= document.getElementById('parola2').value;
-            arrY[0]= y.split("");
-           
-    console.log(arrY);
-    console.log("la prima parola inserita è: " , x );
-    console.log("la seconda parola inserita è: ", y );
-
-}
+            arrX= x.split("");   // tramite split andrò a prendere la stringa e la spezzatto per singolo carattere
+            arrY= y.split("");
+        for( let i=0; i<arrX.length; i++){    // ciclo for che mi analizza tutta la lunghezza dell' array
+            if((arrX[i] === arrX[i].toUpperCase())){ // condizione che verifica la presenza di lettere maiuscole
+            contatoreX +=1;   
+            
+            }else{
+            if(arrX[i]===arrX[i].toLowerCase()){
+                contMinX +=1;
+            }
+            }
+         }  
+        for( let i=0; i<arrY.length; i++){
+            if((arrY[i] === arrY[i].toUpperCase())){
+                contatoreY +=1;
+                
+            }else{
+            if(arrY[i]===arrY[i].toLowerCase()){
+                    contMinY +=1;
+                }
+            }
+        }   
+            console.log("ci sono in totale caratteri maiuscoli: ", contatoreX);  
+            console.log("ci sono in totale caratteri minuscoli: ", contMinX);  
+            console.log("ci sono in totale caratteri maiuscoli: ", contatoreY);  
+            console.log("ci sono in totale caratteri minuscoli: ", contMinY); 
+            }
